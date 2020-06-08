5d7706aa6d196b7b550585681ac2130d6e49ecc7
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-08 21:21:58 +0800

    Add Graph API, WIP: add cognito later

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// import PubSub from '@aws-amplify/pubsub';
+// import API from '@aws-amplify/api';
+import Amplify from 'aws-amplify'
+import awsmobile from './aws-exports';
+
+Amplify.configure(awsmobile);
+// PubSub.configure(awsmobile);
+
 if (environment.production) {
   enableProdMode();
 }
